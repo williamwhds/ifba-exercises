79bfbb76bf63b956f72960e21ff8fd666aa68714
William Oliveira <106339584+williamwhds@users.noreply.github.com>
2023-05-17 04:26:37 +0000

    Métodos desenvolvidos em aluno_disciplina.py

--- Estrutura de Dados/Sistema Aluno Disciplina/SE.py
+++ Estrutura de Dados/Sistema Aluno Disciplina/SE.py
@@ -1,16 +1,19 @@
 #   Algoritmo de Lista Simplemente Encadeada adaptado para funcionamento com sistema_aluno_disciplina.py
 
 class No:
+    '''Este nó acomodará o endereco de memória de um objeto do tipo Aluno.'''
     def __init__(self, valor):
         self.valor = valor
         self.proximo = None
 
 class Lista:
+    '''Esta classe representa uma lista simplesmente encadeada.'''
     def __init__(self):
         self.cabeca = None
         self.tamanho = 0
 
     def adicionar(self, valor):
+        '''Adiciona um nó à lista.'''
         novo_no = No(valor)
         if self.cabeca is None:
             self.cabeca = novo_no
@@ -22,6 +25,7 @@ class Lista:
         self.tamanho += 1
 
     def remover(self, valor):
+        '''Remove um nó da lista.'''
         if self.cabeca is None:
             return
         if self.cabeca.valor == valor:
@@ -35,8 +39,10 @@ class Lista:
                 self.tamanho -= 1
                 return
             no_atual = no_atual.proximo
+        print(f"Não foi possível encontrar o valor {valor}.")
 
     def buscar(self, valor):
+        '''Retorna True se o valor estiver na lista, False caso contrário.'''
         no_atual = self.cabeca
         while no_atual is not None:
             if no_atual.valor == valor:
@@ -45,9 +51,13 @@ class Lista:
         return False
 
     def imprimir(self):
+        '''Imprime os valores da lista.'''
         lista_valores = []
         no_atual = self.cabeca
         while no_atual is not None:
-            lista_valores.append(no_atual.valor)
+            n_aluno = no_atual.valor.matricula
+            nome_aluno = no_atual.valor.nome_aluno
+            lista_valores.append((n_aluno, nome_aluno))
             no_atual = no_atual.proximo
-        print(lista_valores)
+        for aluno in lista_valores:
+            print(f'Nº Matrícula: {aluno[0]} - Nome: {aluno[1]}')
--- Estrutura de Dados/Sistema Aluno Disciplina/aluno_disciplina.py
+++ Estrutura de Dados/Sistema Aluno Disciplina/aluno_disciplina.py
@@ -40,7 +40,7 @@ class Admin:
     def adicionar_disciplina(self, nome_disciplina):
         '''Adiciona uma disciplina na lista de disciplinas.'''
         self.lista_disciplinas.adicionar_no_disciplina(nome_disciplina)
-    
+
     def remover_disciplina(self, nome_disciplina):
         '''Remove uma disciplina da lista de disciplinas.'''
         self.lista_disciplinas.remover_disciplina(nome_disciplina)
@@ -52,3 +52,138 @@ class Admin:
     def remover_aluno(self, n_matricula):
         '''Remove um aluno da lista de alunos.'''
         self.lista_alunos.remover_aluno(n_matricula)
+
+    def adicionar_aluno_disciplina(self, nome_disciplina, n_matricula):
+        '''Adiciona um aluno em uma disciplina.'''
+        no_disciplina = self.lista_disciplinas.buscar_disciplina(nome_disciplina)
+        no_aluno = self.lista_alunos.buscar_aluno(n_matricula)
+        if no_disciplina is None:
+            print(f'Disciplina "{nome_disciplina}" não existe.')
+            return
+        if no_aluno is None:
+            print(f'Aluno com nº "{n_matricula}" não existe.')
+            return
+        no_disciplina.ponteiro.adicionar(no_aluno)
+
+    def remover_aluno_disciplina(self, nome_disciplina, n_matricula):
+        '''Remove um aluno de uma disciplina.'''
+        no_disciplina = self.lista_disciplinas.buscar_disciplina(nome_disciplina)
+        no_aluno = self.lista_alunos.buscar_aluno(n_matricula)
+        if no_disciplina is None:
+            print(f'Disciplina "{nome_disciplina}" não existe.')
+            return
+        if no_aluno is None:
+            print(f'Aluno com nº "{n_matricula}" não existe.')
+            return
+        no_disciplina.ponteiro.remover(no_aluno)
+
+    def imprimir_disciplinas(self):
+        '''Imprime a lista de disciplinas.'''
+        self.lista_disciplinas.imprimir_disciplinas()
+
+    def imprimir_alunos(self):
+        '''Imprime a lista de alunos.'''
+        self.lista_alunos.imprimir_alunos()
+    
+    def imprimir_alunos_em_disciplina(self, nome_disciplina):
+        '''Imprime a lista de alunos de uma disciplina.'''
+        no_disciplina = self.lista_disciplinas.buscar_disciplina(nome_disciplina)
+        if no_disciplina is None:
+            print(f'Disciplina "{nome_disciplina}" não existe.')
+            return
+        no_disciplina.ponteiro.imprimir()
+
+    def imprimir_disciplinas_de_aluno(self, n_matricula):
+        '''Itera sobre as disciplinas e imprime a lista de disciplinas de um aluno.'''
+        no_aluno = self.lista_alunos.buscar_aluno(n_matricula)
+        if no_aluno is None:
+            print(f'Aluno com nº "{n_matricula}" não existe.')
+            return
+        no_disciplina = self.lista_disciplinas.cabeca
+        print(f"Disciplinas de {no_aluno.nome_aluno}:")
+        while no_disciplina is not None:
+            if no_disciplina.ponteiro.buscar(no_aluno):
+                print(no_disciplina.nome_disciplina)
+            no_disciplina = no_disciplina.proximo
+
+'''
+if __name__ == '__main__':
+    admin = Admin()
+    admin.adicionar_disciplina('Estrutura de Dados')
+    admin.adicionar_disciplina('Cálculo')
+    admin.imprimir_disciplinas() # Estrutura de Dados, Cálculo
+
+    admin.adicionar_aluno('João', 1)
+    admin.adicionar_aluno('Maria', 2)
+    admin.imprimir_alunos() # João, Maria
+
+    admin.adicionar_aluno_disciplina('Estrutura de Dados', 1) # João adicionado em Estrutura de Dados
+    admin.adicionar_aluno_disciplina('Estrutura de Dados', 2) # Maria adicionada em Estrutura de Dados
+
+    admin.adicionar_aluno_disciplina('Cálculo', 1) # João adicionado em Cálculo
+
+    admin.imprimir_alunos_em_disciplina('Estrutura de Dados') # João, Maria
+
+    admin.imprimir_disciplinas_de_aluno(1) # Estrutura de Dados, Cálculo
+
+    admin.remover_aluno_disciplina('Estrutura de Dados', 1) # João removido de Estrutura de Dados
+
+    admin.imprimir_disciplinas_de_aluno(1) # Cálculo, já que João foi removido de Estrutura de Dados
+
+    admin.remover_disciplina('Estrutura de Dados') # Estrutura de Dados removida
+
+    admin.imprimir_disciplinas() # Cálculo, já que Estrutura de Dados foi removida
+
+    admin.imprimir_alunos_em_disciplina('Estrutura de Dados') # Disciplina "Estrutura de Dados" não existe.
+
+    admin.imprimir_disciplinas_de_aluno(2) # Nenhuma disciplina, já que Estrutura de Dados foi removida
+'''
+
+admin = Admin()
+_input = ''
+
+while _input != "11":
+    print()
+    print("1 - Adicionar disciplina")
+    print("2 - Remover disciplina")
+    print("3 - Adicionar aluno")
+    print("4 - Remover aluno")
+    print("5 - Adicionar aluno em disciplina")
+    print("6 - Remover aluno de disciplina")
+    print("7 - Imprimir disciplinas")
+    print("8 - Imprimir alunos")
+    print("9 - Imprimir alunos em disciplina")
+    print("10 - Imprimir disciplinas de aluno")
+    print("11 - Sair")
+
+    _input = input("Digite o número da opção desejada: ")
+
+    if _input == "1":
+        admin.adicionar_disciplina(input("Digite o nome da disciplina: "))
+    
+    elif _input == "2":
+        admin.remover_disciplina(input("Digite o nome da disciplina: "))
+    
+    elif _input == "3":
+        admin.adicionar_aluno(input("Digite o nome do aluno: "), input("Digite o número de matrícula do aluno: "))
+
+    elif _input == "4":
+        admin.remover_aluno(input("Digite o número de matrícula do aluno: "))
+
+    elif _input == "5":
+        admin.adicionar_aluno_disciplina(input("Digite o nome da disciplina: "), input("Digite o número de matrícula do aluno: "))
+
+    elif _input == "6":
+        admin.remover_aluno_disciplina(input("Digite o nome da disciplina: "), input("Digite o número de matrícula do aluno: "))
+
+    elif _input == "7":
+        admin.imprimir_disciplinas()
+
+    elif _input == "8":
+        admin.imprimir_alunos()
+
+    elif _input == "9":
+        admin.imprimir_alunos_em_disciplina(input("Digite o nome da disciplina: "))
+
+    elif _input == "10":
+        admin.imprimir_disciplinas_de_aluno(input("Digite o número de matrícula do aluno: "))
