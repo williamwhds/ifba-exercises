7d3d14bfaeb8ed48d5fa78cfb96b29fef457ec4e
William Oliveira <106339584+williamwhds@users.noreply.github.com>
2023-06-10 18:01:30 -0300

    Novos métodos em ab_lse.py
    Métodos para manipulação da lista adicionados

--- Estrutura de Dados/Árvore/AB+LSE/ab_lse.py
+++ Estrutura de Dados/Árvore/AB+LSE/ab_lse.py
@@ -36,3 +36,58 @@ class Admin:
     def imprimir_arvore(self):
         '''Imprime os valores da árvore.'''
         self.arvore.print_tree()
+
+    def imprimir_lista(self, no):
+        '''Imprime os valores da lista. Recebe como parâmetro o nó da árvore.'''
+        no = self.buscar_no_arvore(no)
+        no.lista.imprimir()
+
+    def adicionar_no_lista(self, no, valor):
+        '''Adiciona um nó à lista. Recebe como parâmetro o nó da árvore.'''
+        no = self.buscar_no_arvore(no)
+        no.lista.adicionar(valor)
+
+    def remover_no_lista(self, no, valor):
+        '''Remove um nó da lista. Recebe como parâmetro o nó da árvore.'''
+        no = self.buscar_no_arvore(no)
+        no.lista.remover(valor)
+
+    def buscar_no_lista(self, no, valor):
+        '''Retorna True se o valor estiver na lista, False caso contrário. Recebe como parâmetro o nó da árvore.'''
+        no = self.buscar_no_arvore(no)
+        return no.lista.buscar(valor)
+
+
+if __name__ == "__main__":
+    adm = Admin()
+
+    #Testes da árvore binária
+
+    adicionar = [10, 5, 15, 3, 7, 12, 17, 1, 4, 6, 8, 11, 13, 16, 18]
+    for i in adicionar:
+        adm.adicionar_no_arvore(i)
+    adm.imprimir_arvore() # Adicionando lista de números à árvore
+
+    remover = [10, 5, 15, 3, 7, 12, 17, 1, 4, 6, 8, 11, 13]
+    for i in remover:
+        adm.remover_no_arvore(i)
+    adm.imprimir_arvore() # Sobram 16 e 18
+
+    print(adm.buscar_no_arvore(10)) # Retorna None
+    print(adm.buscar_no_arvore(16)) # Retorna o objeto nó
+
+    #Testes da lista
+
+    adm.adicionar_no_lista(16, 1)
+    adm.adicionar_no_lista(16, 2)
+    adm.adicionar_no_lista(16, 3)
+
+    adm.imprimir_lista(16) # [1, 2, 3]
+    adm.remover_no_lista(16, 2)
+    adm.imprimir_lista(16) # [1, 3]
+
+    print(adm.buscar_no_lista(16, 1)) # True
+    print(adm.buscar_no_lista(16, 2)) # False
+
+    adm.adicionar_no_lista(18, 1)
+    adm.imprimir_lista(18) # [1]
